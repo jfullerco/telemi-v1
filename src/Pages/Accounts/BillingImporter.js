ef401aec252fc9f96b447a199ed5c4c3b32324fc
jfullerco <jonathan@jfuller.co>
2021-03-11 13:08:55 -0500

    2 files modified

--- src/Pages/Accounts/BillingImporter.js
+++ src/Pages/Accounts/BillingImporter.js
@@ -4,9 +4,20 @@ import Tesseract from 'tesseract.js'
 
 const BillingImporter = () => {
   
-  Tesseract.recognize('./billImage.pdf')
-  .then(({data: {text}}) => {
-    console.log(text)
+  const getImage = () => {
+    const billImport = new Image
+    billImport.crossOrigin ="Anonymous"
+    billImport.src = "https://tiems-d1ca.restdb.io/media/604a2da2f9c7885d00003ccb?key=124444446251511451854"
+    return billImport
+  }
+  const bill = getImage()
+
+  const processImage = () => {
+    Tesseract.recognize(bill)
+      .then(console.log())
+  }
+  useEffect(() => {
+    processImage()
   })
   const [ocr, setOcr] = useState('Recognizing...')
   
