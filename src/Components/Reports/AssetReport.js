7981e83088655d12d752d022de6801b0c3dcdc69
jfullerco <jonathan@jfuller.co>
2021-03-16 11:39:00 -0400

    1 file modified

--- src/Components/Reports/AssetReport.js
+++ src/Components/Reports/AssetReport.js
@@ -4,10 +4,10 @@ import {stateContext} from '../../stateContext'
 const AssetReport = () => {
   const userContext = useContext(stateContext)
   const {userSession: {sites}} = userContext
-  
+  const {userSession: {assets}} = userContext
   const [toggleModal, setToggleModal] = useState(true)
   const {site_assets} = sites
-  const assets = site_assets
+  
   const toggleModalClose = () => {
     setToggleModal(false)
   }
@@ -23,7 +23,7 @@ const AssetReport = () => {
               </div>
             </div>
           <div className="card-content">
-            {sites != !sites ? sites.map(site => (
+            {sites != undefined ? sites.map(site => (
             <table className="table is-striped is-fullwidth">
             <thead>
               <tr>
@@ -36,36 +36,35 @@ const AssetReport = () => {
               <tr>
                 <td key={site._id}>{site.site_name}</td> 
               </tr>
-              <tr>
-                <table className="table is-striped is-fullwidth">
-                  <thead>
-                    <tr>
-                      <th>
-                        Asset ID
-                      </th>
-                      <th>
-                        Asset Vendor
-                      </th>
-                      <th>
-                        Asset Type
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {site.site_assets != undefined ? site.site_assets.map(asset => (
-                    <tr key={asset._id}>{console.log(asset)}
-                      <td>{asset.asset_ID}</td> 
-                      <td>{asset.asset_Vendor}</td> 
-                      <td>{asset.asset_Type}</td>
-                    </tr>
-                    )) : (<tr><td>No Assets Added</td></tr>)}
-                  </tbody>
-                </table>
-              </tr>
+                <tr>
+                  <td>
+                    <table className="table is-striped is-fullwidth">
+                      <thead>
+                        <tr>
+                          <th>Asset ID</th>
+                          <th>Asset Vendor</th>
+                          <th>Asset Type</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+
+                      {assets != undefined ? assets.filter((el) => el.hasOwnProperty('asset_siteID') && el.asset_siteID.find((val) => val._id === site._id)).map(asset => (
+                        <tr key={asset._id}>{console.log(asset)}
+                          <td>{asset.asset_ID}</td> 
+                          <td>{asset.asset_Vendor}</td> 
+                          <td>{asset.asset_Type}</td>
+                        </tr>
+                        )) : (
+                          <tr><td>No Assets Added</td></tr>
+                        )}
+                      </tbody>
+                    </table>
+                  </td>
+                </tr>
             </tbody>
           </table>
           )) : (
-            <div>No Assets added</div>
+            <div>No Sites added</div>
           )}
           </div>
         </div>
