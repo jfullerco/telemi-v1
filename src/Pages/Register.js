4a17ca3f10a6c3cf3b01d52cc7b7269dd4f683a1
jfullerco <jonathan@jfuller.co>
2021-03-23 01:02:05 -0400

    1 file modified

--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -1,14 +1,14 @@
 import React, {useContext, useRef, useState} from 'react'
 import {Link, useHistory} from 'react-router-dom'
 
-import {stateContext} from '../stateContext'
 import {useAuth} from '../Contexts/AuthContext'
 
 const Register = (props) => {
-  const userContext = useContext(stateContext)
+  
   const emailRef = useRef()
   const passwordRef = useRef()
   const confirmPasswordRef = useRef()
+
   const {signup} = useAuth()
 
   const [signupError, setSignupError] = useState('')
@@ -38,34 +38,34 @@ const Register = (props) => {
     
     <div className={modalState === true ? "modal is-active" : "modal"}>
     <div className="modal-background"></div>
-    <div className="modal-card">
-    <div className="modal-card-head">Register</div>
-    <div className="modal-card-body">
-        <form>
-          <label>Email</label>
-          <input className="input" type="email" ref={emailRef} />
-          <label>Password</label>
-          <input className="input" type="password" ref={passwordRef}/>
-          <label>Confirm Password</label>
-          <input className="input" type="password" ref={confirmPasswordRef} />
-        </form>
+      <div className="modal-card">
+        <div className="modal-card-head">Register</div>
+        <div className="modal-card-body">
+          <form>
+            <label>Email</label>
+            <input className="input" type="email" ref={emailRef} />
+            <label>Password</label>
+            <input className="input" type="password" ref={passwordRef}/>
+            <label>Confirm Password</label>
+            <input className="input" type="password" ref={confirmPasswordRef} />
+          </form>
         <div className="block">
-        <div className="notification is-danger is-hidden">{signupError}</div>
+          <div className="notification is-danger is-hidden">{signupError}</div>
         </div>
-      <div className="modal-card-foot">
-        
-        <button className={loading !== true ? "button level-item" : "button is-loading"} 
+        <div className="modal-card-foot">
+          
+          <button className={loading !== true ? "button level-item" : "button is-loading"} 
           type="submit" disabled={loading} onClick={handleSubmit}
-        >
+          >
             Register
-        </button>
+          </button>
+          
+          <div className="content is-small">Already registered? <Link to="/dashboard">Login</Link></div>
         
-        <div className="content is-small">Already registered? <Link to="/dashboard">Login</Link></div>
-      
+        </div>
+        <button className="modal-close is-large" aria-label="close" onClick={handleModalClose}></button>  
+        </div>
       </div>
-    <button className="modal-close is-large" aria-label="close" onClick={handleModalClose}></button>  
-    </div>
-    </div>
     </div>
     
   )
