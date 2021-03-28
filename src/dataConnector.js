36bec0a6ba4eb78554e250d9b9f5f57d2e0d9f93
jfullerco <jonathan@jfuller.co>
2021-03-28 10:59:03 -0400

    2 files modified, 1 added

--- src/dataConnector.js
+++ src/dataConnector.js
@@ -1,12 +1,16 @@
-import React from 'react'
+import React, {useState, useContext} from 'react'
 import {db} from './firebase'
 import {useAuth} from '../Contexts/AuthContext'
+import {stateContext} from './stateContext'
 
 const dataConnect = () => {
   
   const currentUser = useAuth()
+  const userContext = useContext(stateContext)
   
   const companiesRef = (currentUser) => {
-    db.collection("Companies").where("UsersAllowed", "==")
+    const fetchUsers = db.collection("Companies").where("Users", "array-contains", currentUser).get()
+    userContext.setClients()
   }
 }
+export {companiesRef}
