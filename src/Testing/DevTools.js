7a42d5b114ee2151e5534e738dc4657a02399cbf
jfullerco <jonathan@jfuller.co>
2021-04-02 10:52:44 -0400

    1 file modified

--- src/Testing/DevTools.js
+++ src/Testing/DevTools.js
@@ -3,10 +3,31 @@ import {stateContext} from '../stateContext'
 
 const DevTools = () => {
   const userContext = useContext(stateContext)
+  const {
+    currentCompanyID,
+    currentCompany,
+    currentLocation,
+    currentService,
+    currentTicket,
+    currentOrder,
+    currentAccount
+  } = userContext.userSession
 
   return (
-    <div className="tile">
-      <div className="code">test</div>
+    <div className="notification is-info">
+      <p className="title is-4">contextView</p>
+      <div className="box">
+
+        <p className="sub-title">
+          currentCompanyID: <code>{currentCompanyID}</code>
+        </p>
+
+        <p className="sub-title">
+          currentCompany: <code>{currentCompany}</code>
+        </p>
+        
+      </div>
+      
     </div>
   )
 }
