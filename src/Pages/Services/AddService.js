fa21834afb8f18113468dd796aa1a027d55664bd
jfullerco <jonathan@jfuller.co>
2021-04-17 23:51:02 -0400

    1 file modified

--- src/Pages/Services/AddService.js
+++ src/Pages/Services/AddService.js
@@ -27,11 +27,14 @@ const AddService = () => {
   const serviceCompanyName = useRef("")
   const serviceMRC = useRef("")
   const serviceDetailsBandwidth = useRef("")
+  const serviceDetailsIPRange = useRef("")
+  const serviceDetailsLANEdgeIP = useRef("")
   const serviceOrderID = useRef("")
   const serviceOrderNum = useRef("")
   const serviceAccountID = useRef("")
   const serviceAccountNum = useRef("")
   const serviceSubAccountNum = useRef("")
+  
 
   useEffect(() => {
     fetchLocations()
@@ -94,30 +97,45 @@ const AddService = () => {
         <div className="modal-card-body">
 
           <form>
-          
-            <label className="label">Service Location</label>
-            <div className="select is-rounded is-fullwidth">
-              <select className="select" onChange={handleChange} ref={serviceLocationID}>
-              {locations != undefined ? locations.map(location => (
-                <option key={location.id} value={location.id} name={location.Name} >
-                  {location.Name}
-                </option>
-              )) : "Add a location before adding a service"}
-              </select>
+
+            <div className="field">
+              <label className="label">Service Location</label>
+              <div className="control">
+                <div className="select is-rounded is-fullwidth">
+                  <select className="select" onChange={handleChange} ref={serviceLocationID}>
+                  {locations != undefined ? locations.map(location => (
+                    <option key={location.id} value={location.id} name={location.Name} >
+                      {location.Name}
+                    </option>
+                  )) : "Add a location before adding a service"}
+                  </select>
+                </div>
+              </div>
             </div>
+
             {toggleQuestions === 1 ? 
             <>
-            <label className="label">Service Name</label>
-            <input className="input is-rounded" type="text" ref={serviceName} />
 
-            <label className="label">Vendor</label>
-            <input className="input is-rounded" type="text" ref={serviceVendor} />
+            <div className="field">
+              <label className="label">Service Name</label>
+              <div className="control">
+                <input className="input is-rounded" type="text" ref={serviceName} />
+              </div>
+            </div>
+
+            <div className="field">
+              <div className="control">
+              <label className="label">Vendor</label>
+                <input className="input is-rounded" type="text" ref={serviceVendor} />
+              </div>
+            </div>
             
             <div className="field">            
             <label className="label">Type</label>
               <div className="control">
                 <div className="select is-rounded is-fullwidth">
                 <select type="select" ref={serviceType} >
+                  <option> </option>
                   <option>Data Only</option>
                   <option>Voice/Data</option>
                   <option>Voice Only</option>
@@ -136,12 +154,50 @@ const AddService = () => {
               </div>
             </div>
 
-            <label className="label">Monthly Cost</label>
-            <input className="input is-rounded" type="text" ref={serviceMRC} />
+            <div className="field">
+              <div className="control">
+              <label className="label">Monthly Cost</label>
+                <input className="input is-rounded" type="text" ref={serviceMRC} />
+              </div>
+            </div>
+
             </> : toggleQuestions === 2 ?
             <>
-            <label className="label">Bandwidth</label>
-            <input className="input is-rounded" type="text" ref={serviceDetailsBandwidth} />
+            <div className="field">
+              <div className="control">
+              <label className="label">Vendor Managed Router</label>
+                <div className="select is-rounded is-fullwidth">
+                  <select type="select" ref={serviceDetailsIPRange}>
+                    <option> </option>
+                    <option>Yes</option>
+                    <option>No</option>
+                  </select>
+                </div>
+              </div>
+            </div>
+
+            <div className="field">
+              <div className="control">
+              <label className="label">Bandwidth</label>
+                <input className="input is-rounded" type="text" ref={serviceDetailsBandwidth} />
+              </div>
+            </div>
+
+            {/** Add Tooltip indicating to use /subnet size when entering. Have function to destructure subnet to provide usable range and gateway address.*/}  
+            <div className="field">
+              <div className="control">
+              <label className="label">Public IP Range</label>
+                <input className="input is-rounded" type="text" ref={serviceDetailsIPRange} />
+              </div>
+            </div>
+
+            <div className="field">
+              <div className="control">
+              <label className="label">LAN Edge IP</label>
+                <input className="input is-rounded" type="text" ref={serviceDetailsLANEdgeIP} />
+              </div>
+            </div>
+
             </> : ""}
           </form>
 
