978dd8c5f9b5e42c179e5ad3c88013dba9305c2f
jfullerco <jonathan@jfuller.co>
2021-04-18 21:18:14 -0400

    1 file modified

--- src/Pages/Dashboard/Components/SiteCard.js
+++ src/Pages/Dashboard/Components/SiteCard.js
@@ -1,8 +1,31 @@
 import React, {useState, useEffect, useContext} from 'react'
 
+import {stateContext} from '../../stateContext'
+import {db} from '../../firebase'
+
 const SiteCard = () => {
+  const userContext = useContext(stateContext)
   return(
-    <></>
+    <>
+    <div className="card">
+
+      <div className="card-header">
+        <p className="card-header-title">Location Name</p>
+      </div>
+
+      <div className="card-image">
+        <figure className="image is-4by3">
+        <img>{/**Location Image from Google will go here */}</img>
+        </figure>
+      </div>
+
+      <div className="card-content">
+      {/**Location data will go here */}
+      </div>
+
+    </div>
+    
+    </>
   )
 }
 export default SiteCard
